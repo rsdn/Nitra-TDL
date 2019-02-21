064629e5cb1c3fadd9ea49abc94a1356959ebb43
Stanislav Matveev <hardcaseminator@gmail.com>
2019-02-22 01:01:35 +0300

    Use Mono.Options to parse command line options.

--- src/Tdl2Json/Tdl2JsonProgram.cs
+++ src/Tdl2Json/Tdl2JsonProgram.cs
@@ -1,29 +1,19 @@
-﻿using Nitra;
+﻿using Mono.Options;
+using Nitra;
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 
 using Tdl2JsonLib;
-
-using File = System.IO.File;
 
 namespace Tdl2Json
 {
     public static class Program
     {
         internal const int MaxErrorsOrWarningsToDisplay = 30;
-        private const string TransformPrompt = "path-to.dll|Qualified.Function.Name";
-
-        private static readonly Dictionary<string, MessageImportance> LogLevels = new Dictionary<string, MessageImportance>(StringComparer.OrdinalIgnoreCase)
-        {
-            ["normal"] = MessageImportance.Low,
-            ["short"]  = MessageImportance.High,
-        };
 
         /// <summary>
         /// Application entry point. Returns 0 for success.
@@ -31,62 +21,39 @@ namespace Tdl2Json
         public static int Main(string[] args)
         {
             var timer = Stopwatch.StartNew();
+            var options = new CommandLineOptions();
             try
             {
-                if (args.Length == 1 && args[0].StartsWith("/from-file:", "-from-file:"))
+                options.Parse(args);
+
+                if (options.NeedHelp)
                 {
-                    var arg               = args[0];
-                    int index             = arg.IndexOf(':') + 1;
-                    var comandOptionsFile = arg.Substring(index, arg.Length - index);
-                    args                  = File.ReadAllLines(comandOptionsFile);
+                    options.PrintHelp(Console.Out);
+                    return 0;
                 }
-                var transformators     = FilterOption(args, "/transformator:", "-transformator:");
-                var outputs            = FilterOption(args, "/out:", "-out:");
-                var workingDirectories = FilterOption(args, "/wd:", "-wd:", "/WorkingDirectory:", "-WorkingDirectory:");
-                var loglevels          = FilterOption(args, "/log-level:", "-log-level:");
-                var files              = ExpandFilePaths(args.Where(a => !a.StartsWith("-", "/out:", "/wd:", "/h", "/?", "/from-file:", "/transformator:", "/log-level:"))).ToArray();
-                var tdls               = files.Where(a => a.EndsWith(".tdl", StringComparison.InvariantCultureIgnoreCase)).ToArray();
-                var refs               = files.Where(a => a.EndsWith(".dll", StringComparison.InvariantCultureIgnoreCase)).ToArray();
-                var needHelp           = args.Contains("/?", "/h", "-?", "-h");
-
-                if (args.Length == 0 || needHelp || outputs.Length > 1 || workingDirectories.Length > 1 || tdls.Length == 0 || loglevels.Length > 1)
-                {
-                    if (!needHelp)
-                        Print("Invalid parameters", ConsoleColor.Red);
-                    Print($"Tdl2Json.exe [-out:output_file.json] file1.tdl[[+file2.tdl]...][[+file3.dll][+file4.dll]...] [-transformator:{TransformPrompt}] [-log-level:normal]", ConsoleColor.Cyan);
-                    Print(@"example Tdl2Json.exe /out:c:\temp\stage_tdl.json c:\TDLs\*.tdl c:\DLLs\Autotest.*.Suites.dll", ConsoleColor.Gray);
-
-                    return needHelp ? 0 : -2;
-                }
-
-                var logLevel = MessageImportance.Low;
-                if (loglevels.Length == 1 && !LogLevels.TryGetValue(loglevels[0], out logLevel))
-                {
-                    Print($"Invalid log level value '{loglevels[0]}'. Valid values are: {string.Join(", ", LogLevels.Keys)}.", ConsoleColor.Cyan);
-                    return -2;
-                }
-
-                var workingDirectory = workingDirectories.SingleOrDefault() ?? Directory.GetCurrentDirectory();
-                var outPath = outputs.SingleOrDefault() ?? Path.Combine(workingDirectory, "out.json");
 
                 JsonGenerator.OnMessage += (message, importance) =>
                 {
-                    if (importance >= logLevel)
+                    if (importance >= options.LogLevel)
                         Print(message, ConsoleColor.DarkGray);
                 };
 
-                var output = new Lazy<TextWriter>(() => new StreamWriter(outPath, false, Encoding.UTF8));
+                foreach (var ignoredOption in options.IgnoredOptions)
+                    PrintError($"File or option was ignored: {ignoredOption}");
+
+                var (tdls, refs) = (options.InputFiles.ToArray(), options.References.ToArray());
+                var output = new Lazy<TextWriter>(() => new StreamWriter(options.OutputFile, false, Encoding.UTF8));
                 CompilerMessageBag messages;
 
-                if (transformators.Length > 0)
+                if (options.Transformers.Count > 0)
                 {
                     Print($"Using custom transformers", ConsoleColor.Magenta);
                     messages = new CompilerMessageBag();
-                    foreach (string transformator in transformators)
+                    foreach (var t in options.Transformers)
                     {
-                        var transformatorFunc = LoadTransformator(transformator);
-                        messages.AddRange(JsonGenerator.Generate(workingDirectory, tdls, refs, isMethodTypingEnabled: true, output: null,
-                            transformatorOutput: outPath, transformatorOpt: transformatorFunc));
+                        var transformatorFunc = LoadTransformator(t.assembly, t.type, t.method);
+                        messages.AddRange(JsonGenerator.Generate(options.WorkingDirectory, tdls, refs, isMethodTypingEnabled: true, output: null,
+                            transformatorOutput: options.OutputFile, transformatorOpt: transformatorFunc));
 
                         if (messages.HasErrors)
                             break;
@@ -94,7 +61,7 @@ namespace Tdl2Json
                 }
                 else
                 {
-                    messages = JsonGenerator.Generate(workingDirectory, tdls, refs, isMethodTypingEnabled: true, output: output,
+                    messages = JsonGenerator.Generate(options.WorkingDirectory, tdls, refs, isMethodTypingEnabled: true, output: output,
                         transformatorOutput: null, transformatorOpt: null);
                 }
 
@@ -104,21 +71,25 @@ namespace Tdl2Json
                 ReportCompilerMessages(messages);
 
                 if (output.IsValueCreated)
-                    Print($"JSON file was created successfully: '{outPath}'.", ConsoleColor.Green);
+                    Print($"JSON file was created successfully: '{options.OutputFile}'.", ConsoleColor.Green);
 
                 Print("Took: " + timer.Elapsed, ConsoleColor.DarkGray);
                 return messages.HasErrors ? -3 : 0;
             }
+            catch (OptionException e)
+            {
+                PrintError(e.Message);
+                options.PrintHelp(Console.Out);
+                return -2;
+            }
             catch (ConfigurationException e)
             {
-                using (new ConsoleForegroundColor(ConsoleColor.Red))
-                    Console.Error.Write($"Configuration error: {e.Message}");
+                PrintError($"Configuration error: {e.Message}");
                 return -3;
             }
             catch (Exception e)
             {
-                using (new ConsoleForegroundColor(ConsoleColor.Red))
-                    Console.Error.Write($"Unhandled exception: {e}");
+                PrintError($"Unhandled exception: {e}");
                 return -3;
             }
         }
@@ -177,107 +148,17 @@ namespace Tdl2Json
             }
         }
 
-        static (string path, string func) getPathAndFunc(string transformator)
+        private static TransfomationFunc LoadTransformator(string path, string typeName, string methodName)
         {
-            var index = transformator.LastIndexOf("|");
-            if (index < 0)
-                ReportIncorrectTransformator();
-            var path = transformator.Substring(0, index);
-            var func = transformator.Substring(index + 1, transformator.Length - (index + 1));
-            return (path, func);
-        }
-
-        static (Type type, string funcName) getTypeAndFunc(Assembly asm, string qualifiedFunctionName)
-        {
-            var index = qualifiedFunctionName.LastIndexOf(".");
-            if (index < 0)
-                ReportIncorrectTransformator();
-            var typeName = qualifiedFunctionName.Substring(0, index);
-            var type     = asm.GetType(typeName, throwOnError: true);
-            var funcName = qualifiedFunctionName.Substring(index + 1, qualifiedFunctionName.Length - (index + 1));
-            return (type, funcName);
-        }
-
-        private static TransfomationFunc LoadTransformator(string transformator)
-        {
-            var (path, qualifiedFunctionName) = getPathAndFunc(transformator);
-            var (type, funcName) = getTypeAndFunc(Assembly.LoadFrom(path), qualifiedFunctionName);
-            return (TransfomationFunc)Delegate.CreateDelegate(typeof(TransfomationFunc), type, funcName);
-        }
-
-        private static void ReportIncorrectTransformator()
-        {
-            throw new Exception($"The 'transformator' parameter should have the format: '{TransformPrompt}'.");
-        }
-
-        private static string[] FilterOption(string[] args, params string[] samples)
-        {
-            return args.Where(a => a.StartsWith(samples)).Select(a => a.Substring(a.IndexOf(':') + 1)).ToArray();
+            var assembly = Assembly.LoadFrom(path);
+            var type = assembly.GetType(typeName, throwOnError: true);
+            return (TransfomationFunc)Delegate.CreateDelegate(typeof(TransfomationFunc), type, methodName);
         }
 
         private static void Print(string text, ConsoleColor color)
         {
             using (new ConsoleForegroundColor(color))
                 Console.WriteLine(text);
-        }
-
-        public static string[] ExpandFilePaths(IEnumerable<string> args)
-        {
-            var fileList = new List<string>();
-
-            foreach (var arg in args)
-            {
-                var substitutedArg = System.Environment.ExpandEnvironmentVariables(arg);
-
-                if (substitutedArg.Contains('*') || substitutedArg.Contains('?'))
-                {
-                    var dirPart = Path.GetDirectoryName(substitutedArg);
-                    if (dirPart.Length == 0)
-                        dirPart = ".";
-
-                    var filePart = Path.GetFileName(substitutedArg);
-
-                    foreach (var filepath in Directory.GetFiles(dirPart, filePart))
-                        fileList.Add(filepath);
-
-                    var dirs = Directory.GetDirectories(dirPart, "*.*", SearchOption.AllDirectories);
-                    foreach (var dir in dirs)
-                        foreach (var filepath in Directory.GetFiles(dir, filePart))
-                            fileList.Add(filepath);
-                }
-                else
-                    fileList.Add(substitutedArg);
-            }
-
-            return fileList.ToArray();
-        }
-
-        public static bool Contains(this string[] args, params string[] samples)
-        {
-            foreach (var arg    in args)
-            foreach (var sample in samples)
-            {
-                if (arg.Contains(sample))
-                    return true;
-            }
-            return false;
-        }
-
-        public static bool StartsWith(this string str, params string[] samples)
-        {
-            foreach (var sample in samples)
-                if (str.StartsWith(sample, StringComparison.OrdinalIgnoreCase))
-                    return true;
-            return false;
-        }
-
-        public static bool StartsWith(this string[] strs, params string[] samples)
-        {
-            foreach (var str    in strs)
-            foreach (var sample in samples)
-                if (str.StartsWith(sample, StringComparison.OrdinalIgnoreCase))
-                    return true;
-            return false;
         }
     }
 }
