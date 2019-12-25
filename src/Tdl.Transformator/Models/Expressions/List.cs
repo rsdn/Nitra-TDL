110e814c928c33c6f5228b83d37434359ab83453
Dmitry Gladyshev <dmitry.gladyshev@kaspersky.com>
2019-12-25 16:49:24 +0300

    Add support of Platforms in products

---
+++ src/Tdl.Transformator/Models/Expressions/List.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace Tdl.Transformator.Models.Expressions
+{
+    public sealed class List : Expression
+    {
+        public List([NotNull] IEnumerable<Expr> list)
+        {
+            ListValue = list;
+        }
+
+        public IEnumerable<Expr> ListValue { get; set; }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.Append("[");
+            foreach (var value in ListValue)
+            {
+                builder.Append($"{value}");
+                if (value != ListValue.Last())
+                {
+                    builder.Append(", ");
+                }
+            }
+            builder.Append("];");
+
+
+
+            return builder.ToString();
+        }
+
+        public override bool Equals(Expression other)
+        {
+            return other is List second && ListValue.SequenceEqual(second.ListValue); 
+        }
+    }
+}
