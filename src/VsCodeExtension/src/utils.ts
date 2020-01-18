766f059c8a4f65a114a3049f7062f19b870f1eff
VladD2 <vc@rsdn.ru>
2020-01-18 03:14:37 +0300

    [ClientServer][LSP] Work on project system.

--- src/VsCodeExtension/src/utils.ts
+++ src/VsCodeExtension/src/utils.ts
@@ -3,11 +3,21 @@ import * as vscode from 'vscode';
 export const ExtentionName = "TDL extension";
 
 export function showMessage(text: string): void {
-  console.log(text);
+  log(text);
   vscode.window.showInformationMessage(text);
 }
 
 export function showError(text: string): void {
-  console.error(text);
+  error(text);
   vscode.window.showErrorMessage(text);
 }
+
+export function log(text: string): void {
+  console.log(date.toISOString() + " " + text);
+}
+
+export function error(text: string): void {
+  console.error(date.toISOString() + " " + text);
+}
+
+const date = new Date();
