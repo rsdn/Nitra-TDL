84ed272952ada3211a363b22a54d1b922f49b218
VladD2 <vc@rsdn.ru>
2020-02-25 18:21:22 +0300

    Update build batch files.

--- src/Build.cmd
+++ src/Build.cmd
@@ -1,7 +1,22 @@
-SET MSBUILDENABLEALLPROPERTYFUNCTIONS=1
+title %~nx0
+
 SET Config=Debug
 SET Verbosity=m
+SET Root=%~dp0..\..
+SET MSBUILDENABLEALLPROPERTYFUNCTIONS=1
 
-msbuild Tdl.sln /p:NemerleBinPathRoot=c:\RSDN\nemerle\bin\%Config% /t:Restore /v:%Verbosity%
-msbuild Tdl.sln /p:NemerleBinPathRoot=c:\RSDN\nemerle\bin\%Config% /v:%Verbosity%
-rem pause
+@echo %Root%
+
+setlocal ENABLEEXTENSIONS
+set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7"
+set VALUE_NAME=15.0
+
+for /f "tokens=2,*" %%a in ('reg query %KEY_NAME% /V %VALUE_NAME%  ^|findstr /ri "REG_SZ"') DO set Value=%%b
+
+IF defined Value (
+call "%Value%Common7\Tools\VsDevCmd.bat"
+set NoPause=true
+
+msbuild %~dp0Tdl.sln /p:NemerleBinPathRoot=%Root%\nemerle\bin\%Config% /t:Restore /v:%Verbosity%
+msbuild %~dp0Tdl.sln /p:NemerleBinPathRoot=%Root%\nemerle\bin\%Config% /v:%Verbosity%
+)
